8513f0b3a953c4e8aadf6f76b20ae0d2f6e6da9c
Chinmay0502 <chinamayakumarsahoo@gmai.com>
2026-03-07 11:01:30 +0530

    feat(auth): implement authentication frontend (login & register pages)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { Bounce, ToastContainer, toast } from "react-toastify";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+import axios from "axios";
+import Services from "./pages/Services";
+import AdminLogin from "./pages/AdminLogin";
+import Login from "./pages/Login";
+
 
 const App = () => {
-  return (
-    <div className="text-center text-2xl bg-rose-300">Servio</div>
-  )
-}
+  const [location, setLocation] = useState();
+  const [openDropdown, setOpenDropdown] = useState(false);
+  const getLocation = async () => {
+    navigator.geolocation.getCurrentPosition(async (pos) => {
+      const { latitude, longitude } = pos.coords;
+      console.log(latitude, longitude);
 
-export default App
+      const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`;
+
+      try {
+        const location = await axios.get(url);
+        // console.log(location);
+        const address = location.data.address;
+        // console.log(address);
+        setLocation(address);
+        setOpenDropdown(false);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  };
+
+  useEffect(() => {
+    toast.success("Hi toast is working");
+    getLocation();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Bounce}
+      />
+      <Navbar
+        location={location}
+        getLocation={getLocation}
+        openDropdown={openDropdown}
+        setOpenDropdown={setOpenDropdown}
+      />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/admin/login" element={<AdminLogin />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signin" element={<Signin />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/services" element={<Services />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
